d4e9cf87b2647186f5c989d001fc0880fa038f3c
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-12-19 18:21:43 +0900

    feat: TravelCostLayout 설정

--- Polarnal/DB/DBModel/Travel/Type/TravelCostType.swift
+++ Polarnal/DB/DBModel/Travel/Type/TravelCostType.swift
@@ -5,7 +5,7 @@
 //  Created by 최승범 on 12/16/24.
 //
 
-import Foundation
+import SwiftUI
 
 enum TravelCostType: String, CaseIterable {
     case hotel
@@ -28,7 +28,7 @@ enum TravelCostType: String, CaseIterable {
         case .shopping:
             return "쇼핑"
         case .flight:
-            return "비행기"
+            return "항공"
         case .transportation:
             return "교통비"
         case .activity:
@@ -41,6 +41,31 @@ enum TravelCostType: String, CaseIterable {
             return "추가 비용"
         case .other:
             return "기타"
+        }
+    }
+    
+    var textIcon: String {
+        switch self {
+        case .hotel:
+            return "🏠"
+        case .meals:
+            return "🍽️"
+        case .shopping:
+            return "🛍️"
+        case .flight:
+            return "✈️"
+        case .transportation:
+            return "🚋"
+        case .activity:
+            return "🎯"
+        case .culturalActivities:
+            return "🎬"
+        case .gift:
+            return "🎁"
+        case .extrafee:
+            return "💸"
+        case .other:
+            return "👾"
         }
     }
     
@@ -66,6 +91,31 @@ enum TravelCostType: String, CaseIterable {
             return "plus.circle.fill"
         case .other:
             return "questionmark.circle.fill"
+        }
+    }
+    
+    var color: Color {
+        switch self {
+        case .hotel:
+            return Color(red: 0.60, green: 1.0, blue: 0.60) // 부드러운 파스텔 그린 (호텔)
+        case .meals:
+            return Color(red: 0.75, green: 0.89, blue: 0.72) // 파스텔 그린 (식사)
+        case .shopping:
+            return Color(red: 0.98, green: 0.74, blue: 0.55) // 파스텔 오렌지 (쇼핑)
+        case .flight:
+            return Color(red: 0.67, green: 0.88, blue: 0.98) // 파스텔 스카이 블루 (항공)
+        case .transportation:
+            return Color(red: 0.80, green: 0.80, blue: 0.80) // 파스텔 그레이 (교통)
+        case .activity:
+            return Color(red: 0.80, green: 0.70, blue: 0.90) // 파스텔 퍼플 (활동)
+        case .culturalActivities:
+            return Color(red: 1.00, green: 0.98, blue: 0.55) // 파스텔 옐로우 (문화활동)
+        case .gift:
+            return Color(red: 0.98, green: 0.82, blue: 0.87) // 파스텔 핑크 (선물)
+        case .extrafee:
+            return Color(red: 1.00, green: 0.60, blue: 0.60) // 파스텔 레드 (추가 요금)
+        case .other:
+            return Color(red: 0.80, green: 0.80, blue: 0.85) // 파스텔 그레이 (기타)
         }
     }
 }
--- Polarnal/Present/TravelPlanner/TravelDashboard/TravelCost/TravelCostView.swift
+++ Polarnal/Present/TravelPlanner/TravelDashboard/TravelCost/TravelCostView.swift
@@ -12,21 +12,16 @@ struct TravelCostView: View {
         
         VStack {
             HStack {
-                RoundedRectangle(cornerRadius: 24)
+                TravelCostOverView()
                     .background(Color(uiColor: .systemGray5))
                     .clipShape(RoundedRectangle(cornerRadius: 24))
                     .padding()
                 
                 
-                
-                List {
-                    Text("ㅇ아ㅏ아ㅏ")
-                    Text("ㅇ아ㅏ아ㅏ")
-                    Text("ㅇ아ㅏ아ㅏ")
-                }
-                .background(Color(uiColor: .systemGray5))
-                .clipShape(RoundedRectangle(cornerRadius: 24))
-                .padding()
+                TravelCostListView()
+                    .background(Color(uiColor: .systemGray5))
+                    .clipShape(RoundedRectangle(cornerRadius: 24))
+                    .padding()
             }
             
         }
@@ -34,7 +29,85 @@ struct TravelCostView: View {
     }
 }
 
+struct TravelCostOverView: View {
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .frame(width: 400, height: 400)
+                Circle()
+                    .fill(Color(uiColor: .systemGray5))
+                    .frame(width: 200, height: 200)
+                Text("1,523,500 ₩")
+                    .font(.title)
+                    .bold()
+                    
+                    
+            }
+            
+            List {
+                ForEach(TravelCostType.allCases, id: \.self) { type in
+                    costTypeCell(type: type,
+                                 percent: 24.8)
+                }
+            }
+        }
+        
+    }
+    
+    
+    private func costTypeCell(type: TravelCostType,
+                              percent: Double) -> some View {
+        HStack {
+            Circle()
+                .fill(type.color)
+                .frame(width: 16, height: 16)
+            
+            Text(type.title)
+                .padding(.leading)
+            
+            Text(String(format: "%.1f", percent) + "%")
+            Spacer()
+            Text(type.textIcon)
+        }
+    }
+}
 
+struct TravelCostListView: View {
+    var body: some View {
+        List {
+            costCell()
+            costCell()
+            costCell()
+        }
+    }
+    
+    private func costCell() -> some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 44, height: 44)
+                .padding()
+            
+            Text("인천 공항 -> 이스탄불 공항")
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("542,900 ₩")
+                    .font(.title3)
+                    .bold()
+                Text("24.02.12")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+                    .padding(.top, 4)
+            }
+            .padding(8)
+        }
+    }
+}
+
+#Preview {
+    TravelCostView()
+}
 
 // 날짜
 // 사진
