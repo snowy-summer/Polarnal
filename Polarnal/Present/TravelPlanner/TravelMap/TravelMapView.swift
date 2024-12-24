2e8b8c6a0b9db98ac1dc3cc114fc8d52545f7405
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-12-24 20:45:26 +0900

    feat: TravelMapView 추가

---
+++ Polarnal/Present/TravelPlanner/TravelMap/TravelMapView.swift
@@ -0,0 +1,15 @@
+//
+//  TravelMapView.swift
+//  Polarnal
+//
+//  Created by 최승범 on 12/24/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TravelMapView: View {
+    var body: some View {
+        Text("이")
+    }
+}
